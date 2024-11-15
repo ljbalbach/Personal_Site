29289ac777b02dae9ec7357d4b815bfedfb198e3
Lucas Balbach <lucasbalbach@Lucass-MacBook-Air.local>
2024-11-15 09:57:03 +0000

    removing unused package

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:math';
 
-import 'package:expansion_tile_list/expansion_tile_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_starter/containers/default_container.dart';

b2db7c90c939e97c6a0f5f5bd2ec8e4a368b0a93
Lucas Balbach <lucasbalbach@Lucass-MacBook-Air.local>
2024-11-18 14:04:19 +0000

    new logo and updated assets structure to be more clean

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,16 +1,19 @@
 // ignore_for_file: constant_identifier_names
 
 //BACKGROUNDS
-const String WINTER = "winter.png";
-const String SUMMER = "summer.jpeg";
-const String SEASONS = "seasons.png";
+const String WINTER = "backgrounds/winter.png";
+const String SUMMER = "backgrounds/summer.jpeg";
+const String SEASONS = "backgrounds/final_seasons.png";
 
 //SOCIALS
-const String GITHUB = "github.svg";
-const String LINKEDIN = "linkedin.svg";
+const String GITHUB = "logos/github.svg";
+const String LINKEDIN = "logos/linkedin.svg";
 
 //EXPERIENCE
-const String VISA = "visa.png";
-const String CALSPAN_BLACK = "calspan_black.png";
-const String CALSPAN_WHITE = "calspan_white.png";
-const String COACHMEPLUS = "coachmeplus.png";
+const String VISA = "logos/visa.png";
+const String CALSPAN_BLACK = "logos/calspan_black.png";
+const String CALSPAN_WHITE = "logos/calspan_white.png";
+const String COACHMEPLUS = "logos/coachmeplus.png";
+
+//PROJECTS
+const String PROJECTS = "projects/";

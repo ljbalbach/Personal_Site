a5655eb948ef7ee28c97b227c349d99fcb91a3d8
Lucas Balbach <lucasbalbach@Lucass-Air.lan>
2024-12-11 12:55:25 +0000

    finishing touches

--- lib/containers/default_container.dart
+++ lib/containers/default_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_starter/model/locale.dart';
 import 'package:flutter_starter/utils/common.dart';
@@ -18,46 +19,50 @@ class DefaultContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var loc = AppLocalizations.of(context);
+    Color backgroundColor = lightMode ? Colors.white : Colors.black;
 
-    return Scaffold(
-      backgroundColor: lightMode ? Colors.white : Colors.black,
-      body: Stack(
-        children: [
-          if (background != null)
-            background!,
-          SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                body,
-              ],
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle(statusBarColor: backgroundColor),
+      child: Scaffold(
+        backgroundColor: backgroundColor,
+        body: Stack(
+          children: [
+            if (background != null)
+              background!,
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  body,
+                ],
+              ),
             ),
-          ),
-          Positioned(
-            top: _defaultPadding.top,
-            right: _defaultPadding.right,
-            child: Consumer<LocaleModel>(
-              builder: (context, localeModel, child) =>
-                RoundButton(
-                  onPressed: () => localeModel.swap(),
-                  backgroundColor: lightMode ? Colors.black : Colors.white,
-                  child: Text(loc.language, style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: lightMode ? Colors.white : Colors.black)),
-                ),
-            ),
-          ),
-          if (bottomLeft != null)
             Positioned(
-              bottom: _defaultPadding.bottom,
-              left: _defaultPadding.left,
-              child: bottomLeft!,
-            ),
-          if (bottomRight != null)
-            Positioned(
-              bottom: _defaultPadding.bottom,
+              top: _defaultPadding.top,
               right: _defaultPadding.right,
-              child: bottomRight!,
+              child: Consumer<LocaleModel>(
+                builder: (context, localeModel, child) =>
+                  RoundButton(
+                    onPressed: () => localeModel.swap(),
+                    backgroundColor: lightMode ? Colors.black : Colors.white,
+                    child: Text(loc.language, style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: backgroundColor)),
+                  ),
+              ),
             ),
-        ],
+            if (bottomLeft != null)
+              Positioned(
+                bottom: _defaultPadding.bottom,
+                left: _defaultPadding.left,
+                child: bottomLeft!,
+              ),
+            if (bottomRight != null)
+              Positioned(
+                bottom: _defaultPadding.bottom,
+                right: _defaultPadding.right,
+                child: bottomRight!,
+              ),
+          ],
+        ),
       ),
     );
   }
